d6f38cdc7a561cc4d8f1853ef5d7f6401818bb6c
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2022-11-18 18:21:18 +0600

    Create quotient&Reminder.cpp

---
+++ quotient&Reminder.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int x,y;
+   cin>>x>>y;
+   int quotient=(x/y);
+   int reminder=(x%y);
+
+   cout<<"Quotient is "<<quotient<<"\n";
+   cout<<"Reminder is "<<reminder<<"\n";
+
+    return 0;
+}
