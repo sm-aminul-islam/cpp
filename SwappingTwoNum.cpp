5ed511e457f6c0304a0cde49ab141e38fc46c1fd
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2022-11-18 18:28:04 +0600

    Create SwappingTwoNum.cpp

---
+++ SwappingTwoNum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int x,y;
+   cout<<"Before swapping : ";
+   cin>>x>>y;
+   int temp=x;
+   x=y;
+   y=temp;
+   cout<<"After swapping  :"<< x<<" "<<y;
+
+    return 0;
+}
