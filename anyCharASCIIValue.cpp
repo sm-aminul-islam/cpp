991708d597c940aac4f96ed9ff5a7915e48b1981
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2022-11-18 19:12:09 +0600

    Create anyCharASCIIValue.cpp

---
+++ anyCharASCIIValue.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ char c;
+ cout<<"Character is :"<<"\n";
+ cin>>c;
+ cout<<"ASCII value of  " <<c<<"="<<int (c)<<"\n";
+    return 0;
+}
