be112c9de03ad40e6dd6c99a480fb4f34dc957f3
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2022-11-18 19:04:11 +0600

    Create datatypeSize.cpp

---
+++ datatypeSize.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  cout<<"size of character data type :"<<sizeof(char)<<"byte"<<"\n";
+  cout<<"size of int  data type :"<<sizeof(int )<<"bytes"<<"\n";
+  cout<<"size of double data type :"<<sizeof(double)<<"bytes"<<"\n";
+  cout<<"size of float data type :"<<sizeof(float)<<"bytes"<<"\n";
+    return 0;
+}
